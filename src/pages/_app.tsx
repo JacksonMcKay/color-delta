fd38bd14851bd8e29955fecc998ff1707dab4ec1
Jackson McKay <jackson.t.mckay@gmail.com>
2024-03-04 02:18:01 +1100

    Default to dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,11 @@ import { Layout } from '@/components/Layout';
 import '@/styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import theme from '../../theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
