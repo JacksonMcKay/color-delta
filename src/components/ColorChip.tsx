b6de0f54b7752fa508ac6001d49c0b6059ada875
Jackson McKay <jackson.t.mckay@gmail.com>
2024-03-03 20:54:00 +1100

    Retire the checkered pattern for now

--- src/components/ColorChip.tsx
+++ src/components/ColorChip.tsx
@@ -1,9 +1,7 @@
-import styles from './ColorChip.module.scss';
-
 export function ColorChip({ color }: { color?: string }) {
   return (
     <div className="relative w-[90px] h-[90px]">
-      <div className={`${styles.checkers} absolute inset-0`}></div>
+      <div className={`absolute inset-0 bg-white`}></div>
       <div
         className="absolute inset-0"
         style={{ backgroundColor: color }}
